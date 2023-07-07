e07d7f89c1867f9ff80de26ecd9f92d50a52fae4
Wojciech Kaczmarczyk <wkn@wp.pl>
2023-07-07 20:24:42 +0000

    fi x tag and push images script

--- scripts/tag_and_push_images.sh
+++ scripts/tag_and_push_images.sh
@@ -4,10 +4,10 @@
 read -p "ECR URI : " ECR_URI
 echo "ECR_URI: $ECR_URI"
 
-docker tag flask-demo:2.0.2 $ECR_URI/flask-demo:2.0.0
-docker tag flask-demo:2.0.2 $ECR_URI/flask-demo:2.0.1
-docker tag flask-demo:2.0.2 $ECR_URI/flask-demo:2.0.2
+docker tag flask-demo:2.0.2 $ECR_URI:2.0.0
+docker tag flask-demo:2.0.2 $ECR_URI:2.0.1
+docker tag flask-demo:2.0.2 $ECR_URI:2.0.2
 
-docker push $ECR_URI/flask-demo:2.0.0
-docker push $ECR_URI/flask-demo:2.0.1
-docker push $ECR_URI/flask-demo:2.0.2
+docker push $ECR_URI:2.0.0
+docker push $ECR_URI:2.0.1
+docker push $ECR_URI:2.0.2
