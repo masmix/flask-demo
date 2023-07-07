d31cddf87596d7f58204da80f57b5b61f21a6f51
Wojciech Kaczmarczyk <wkn@wp.pl>
2023-07-07 19:38:16 +0000

    u

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,15 @@ resource "aws_ecrpublic_repository" "flask-demo" {
   repository_name                 = "flask-demo"
   
 }
+
+resource "aws_ecr_registry_scanning_configuration" "configuration" {
+  scan_type = "ENHANCED"
+
+  rule {
+    scan_frequency = "CONTINUOUS_SCAN"
+    repository_filter {
+      filter      = "example"
+      filter_type = "WILDCARD"
+    }
+  }
+}
