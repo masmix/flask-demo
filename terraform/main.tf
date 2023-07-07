d65d6da42e1d52e027adafc327ba788578e69db8
Wojciech Kaczmarczyk <wkn@wp.pl>
2023-07-07 19:51:17 +0000

    u

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,8 @@
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
+resource "aws_ecr_repository" "test" {
+  name                 = "test"
+  image_tag_mutability = "MUTABLE"
 
-resource "aws_ecrpublic_repository" "flask-demo" {
-  repository_name                 = "flask-demo"
-  provider = aws.us_east_1
-}
-
-resource "aws_ecr_registry_scanning_configuration" "configuration" {
-  scan_type = "ENHANCED"
-
-  rule {
-    scan_frequency = "CONTINUOUS_SCAN"
-    repository_filter {
-      filter      = "example"
-      filter_type = "WILDCARD"
-    }
+  image_scanning_configuration {
+    scan_on_push = true
   }
 }
