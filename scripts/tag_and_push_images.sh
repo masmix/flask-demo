ad5bf509d7c0d6c4077b22a8bfb36cbc6520ade7
Wojciech Kaczmarczyk <wkn@wp.pl>
2023-07-07 20:28:03 +0000

    fix tag and update script

--- scripts/tag_and_push_images.sh
+++ scripts/tag_and_push_images.sh
@@ -4,8 +4,8 @@
 read -p "ECR URI : " ECR_URI
 echo "ECR_URI: $ECR_URI"
 
-docker tag flask-demo:2.0.2 $ECR_URI:2.0.0
-docker tag flask-demo:2.0.2 $ECR_URI:2.0.1
+docker tag flask-demo:2.0.0 $ECR_URI:2.0.0
+docker tag flask-demo:2.0.1 $ECR_URI:2.0.1
 docker tag flask-demo:2.0.2 $ECR_URI:2.0.2
 
 docker push $ECR_URI:2.0.0
