bf92dcdfcd57bc7044335999c48e9981ff532aa5
Wojciech Kaczmarczyk <wkn@wp.pl>
2023-07-07 19:29:50 +0000

    tf update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,9 @@
-resource "aws_ecr_repository" "flask-demo" {
+resource "aws_ecrpublic_repository" "flask-demo" {
   name                 = "flask-demo"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
+  
 }
